42d1071066affd278169bcab17d1a975d3ce1bec
Shérab <Sebastien.Hinderer@ens-lyon.org>
2014-10-12 09:36:22 +0200

    By default, th synthe3 executable should use the sound backend rather than outputting a WAV file.

--- main.cpp
+++ main.cpp
@@ -28,7 +28,8 @@ int main( int argc, char** argv )
 	//Prononce une phrase avec les paramètres par défaut (à -1) : volume, débit, hauteur, phonétique, modeLec, compta
 	//puis 0 (pas de son) puis 1 (sortie wave), puis "essai.wav"
 	//	synTexte("Ceci est une phrase que je prononce correctement.", -1, -1, -1, -1, -1, -1, 0, 1, "/tmp/essaiSynthe1.wav");
-		synTexte("Ceci est une phrase que je prononce correctement.", -1, -1, -1, -1, -1, -1, 0, 1);
+	//	synTexte("Ceci est une phrase que je prononce correctement.", -1, -1, -1, -1, -1, -1, 0, 1);
+		synTexte("Ceci est une phrase que je prononce correctement.", -1, -1, -1, -1, -1, -1, 1, 0);
 	while(synIndex()>0);	//attend la fin de la phrase (sinon coupe)
 	/*	synTexte("Et la suivante.", -1, -1, -1, -1, -1, -1, 0, 1, "/tmp/essaiSynthe2.wav");
 		while(synIndex()>0);	//attend la fin de la phrase (sinon coupe)
